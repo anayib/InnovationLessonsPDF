796bd06e521db12a74c4e8ca7d1af1b2fe105629
anayib <nayib.abdala@gmail.com>
2015-02-24 21:23:17 -0500

    Integrates google docs form for those clicking download free sample strategy

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -8,4 +8,8 @@ class LandingPagesController < ApplicationController
 
   end
 
+  def wireform
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'landing_pages#landing_A'
 
   get  'landing_pages/subscribe'
+
+  get 'landing_pages/wireform'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
